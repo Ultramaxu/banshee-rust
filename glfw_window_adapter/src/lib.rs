d3b549759f4906038fdffb8a8339004d9dc93bd4
ultramaxu <ultramaxu@proton.me>
2024-07-26 08:55:59 +0200

    GLFW setup

--- glfw_window_adapter/src/lib.rs
+++ glfw_window_adapter/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Context;
+
+pub struct GLFWWrapper {
+    glfw: glfw::Glfw,
+    window: glfw::PWindow,
+    events: glfw::GlfwReceiver<(f64, glfw::WindowEvent)>
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl GLFWWrapper {
+    pub fn new() -> anyhow::Result<GLFWWrapper> {
+        use glfw::fail_on_errors;
+        let mut glfw = glfw::init(fail_on_errors!())
+            .context("Failed to initialize GLFW.")?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // Disable default OpenGL
+        glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
+
+        let (mut window, events) = glfw
+            .create_window(800, 600, "Hello this is window", glfw::WindowMode::Windowed)
+            .context("Failed to create GLFW window.")?;
+
+        window.set_key_polling(true);
+
+        Ok(GLFWWrapper {
+            glfw,
+            window,
+            events
+        })
+    }
+
+    pub fn poll_events<F>(&mut self, handle_event: F) where F: Fn(&mut glfw::Window, glfw::WindowEvent) {
+        self.glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&self.events) {
+            handle_event(&mut self.window, event);
+        }
+    }
+    
+    pub fn should_loop_continue(&mut self) -> bool {
+        !self.window.should_close()
     }
 }
--- test_target_desktop/src/main.rs
+++ test_target_desktop/src/main.rs
@@ -1,3 +1,23 @@
+use glfw_window_adapter::GLFWWrapper;
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw_wrapper = match GLFWWrapper::new() { 
+        Ok(glfw_wrapper) => glfw_wrapper,
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return;
+        }
+    };
+    
+    while glfw_wrapper.should_loop_continue() {
+        glfw_wrapper.poll_events(|window, event| {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }
+        });
+    }
 }
